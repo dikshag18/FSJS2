bcc84c881dbcb715f8c637ca3a32898491845f7c
dikshag18 <guptadiksha203@gmail.com>
2024-03-22 18:06:24 +0530

    Initial Commit

---
+++ JavaScript/que1/variable.js.js
@@ -0,0 +1,13 @@
+
+// Declaring variables and assign values of different data types
+var myString = "This is a string";
+var myBoolean = true;
+var myUndefined = undefined;
+var myNull = null;
+
+// Display the values along with their data types
+console.log("Value of myString:", myString, "Data Type", typeof myString);
+console.log("Value of myBoolean:", myBoolean, "Data Type", typeof myBoolean);
+console.log("Value of myUndefined:", myUndefined, "Data Type", typeof myUndefined);
+console.log("Value of myNull:", myNull, "Data Type", typeof myNull);
+
