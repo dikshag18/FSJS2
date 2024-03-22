13aae27b4ff179e3b58c56a324a4d33279d4ff4f
dikshag18 <guptadiksha203@gmail.com>
2024-03-22 18:22:05 +0530

    Initial Commit

---
+++ JavaScript/Ques 4/first.js
@@ -0,0 +1,14 @@
+
+// Declaring a variable and assign a string value to it
+var myString = "This is a JavaScript example";
+
+// Check if the string contains the word "Script"
+var containsScript = myString.includes("Script");
+
+// Display the result
+if (containsScript) {
+  console.log("The string contains the word 'Script'.");
+} 
+else {
+  console.log("The string does not contain the word 'Script'.");
+}
