5f5b61b20bfd505b86b1a2a877ccb9c58c393947
dikshag18 <guptadiksha203@gmail.com>
2024-03-22 18:11:29 +0530

    Initial Commit

---
+++ JavaScript/Ques 2/first.js
@@ -0,0 +1,14 @@
+
+// Declaring variables to store personal information
+const firstName = "Tushar";
+const lastName = "Wanekar";
+const maritalStatus = "Single";
+const country = "India";
+const age = 24;
+
+// Display personal information using interpolation (template literals)
+console.log(`First Name: ${firstName}`);
+console.log(`Last Name: ${lastName}`);
+console.log(`Marital Status: ${maritalStatus}`);
+console.log(`Country: ${country}`);
+console.log(`Age: ${age}`);
