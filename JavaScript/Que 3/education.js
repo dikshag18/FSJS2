13fe00e857870d6a12dfda764338aeb054fa252c
dikshag18 <guptadiksha203@gmail.com>
2024-03-22 18:17:28 +0530

    Initial Commit

---
+++ JavaScript/Que 3/education.js
@@ -0,0 +1,10 @@
+
+// Declare a variable and assign a string value to it
+var myString = "hello world! This is JavaScript string.";
+
+// Convert all characters to capital letters
+var uppercaseString = myString.toUpperCase();
+
+// Display the result
+console.log("Original String:", myString);
+console.log("Capital String:", uppercaseString);
