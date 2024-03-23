3228567a3580de97ef53e38e49f664703b03fc57
dikshag18 <guptadiksha203@gmail.com>
2024-03-23 17:00:03 +0530

    Initial Commit

---
+++ JavaScript/Ques 5/script.js
@@ -0,0 +1,9 @@
+
+// Declaring a variable and assign a string value to it
+var myString = "Hello, world! This is a demonstration.";
+
+// Split the string into an array using the split() method
+var myArray = myString.split(" ");
+
+// Output
+console.log(myArray);
