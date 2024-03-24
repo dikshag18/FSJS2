fd119429499df5d7ca6a2b2e59e54e6d21815710
dikshag18 <guptadiksha203@gmail.com>
2024-03-24 17:58:56 +0530

    Initial Commit

---
+++ JavaScript/questions/que10.js
@@ -0,0 +1,26 @@
+
+
+
+console.log(4 > 3);
+console.log(4 >= 3);
+console.log(4 < 3);
+console.log(4 <= 3);
+console.log(4 == 4);
+console.log(4 === 4);
+console.log(4 != 4);
+console.log(4 !== 4);
+console.log(4 != '4');
+console.log(4 == '4');
+console.log(4 === '4');
+
+//length od python and jargon
+
+var mystring = "python";
+console.log(mystring.length);
+
+var string = "jargon";
+console.log(mystring.length);
+
+console.log("python".length > "jargon".length); 
+
+
