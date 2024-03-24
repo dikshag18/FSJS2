96e642a06c0b7b043b99bd210c4a25f8b8f71f75
dikshag18 <guptadiksha203@gmail.com>
2024-03-24 17:46:01 +0530

    Initial Commit

---
+++ JavaScript/questions/que9.js
@@ -0,0 +1,21 @@
+
+//Truthy value and falsy values
+
+var truthyString = "Hello world!";
+console.log(Boolean(truthyString)); 
+
+let truthyNumber = 65;
+console.log(Boolean(truthyNumber)); 
+
+let truthyArray = [1, 2, 3, 4, 5];
+console.log(Boolean(truthyArray)); 
+
+var falsyString = "";
+console.log(Boolean(falsyString)); 
+
+let falsyNumber = 0;
+console.log(Boolean(falsyNumber)); 
+
+var falsyVariable;
+console.log(Boolean(falsyVariable));
+
